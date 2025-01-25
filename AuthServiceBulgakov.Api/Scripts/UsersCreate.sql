759029d637070c624d6e2ad6662690dc43b17719
mausermuz <den-bulga@yandex.ru>
2025-01-25 17:01:33 +0300

    Добавил скрипт для заполнения юзеров в момент запуска миграций

---
+++ AuthServiceBulgakov.Api/Scripts/UsersCreate.sql
@@ -0,0 +1,11 @@
+INSERT INTO dbo.Users
+(Id, UserName, IsActive, PasswordHash, Email)
+VALUES
+--(NEWID(), 'Denis', 0, '1234', ''),
+(NEWID(), 'Viktor', 0, '1234', ''),
+(NEWID(), 'Maks', 0, '1234', ''),
+(NEWID(), 'Oleg', 1, '1234', ''),
+(NEWID(), 'Stepan', 1, '1234', ''),
+(NEWID(), 'Anna', 0, '1234', ''),
+(NEWID(), 'Sergey', 1, '1234', ''),
+(NEWID(), 'Tolya', 1, '1234', '')
---
+++ AuthServiceBulgakov.DataAccess.MSSQL/Migrations/20250125130607_SQLScript_Migration_AddUsers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Reflection;
+
+#nullable disable
+
+namespace AuthServiceBulgakov.DataAccess.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class SQLScript_Migration_AddUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var test = Path.GetFullPath(@"Scripts\UsersCreate.sql");
+            migrationBuilder.Sql(File.ReadAllText(test));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
